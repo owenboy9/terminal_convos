b05e1c9534ef1561b6fb8e68353d9b493e1ae784
owenboy9 <owenkraus9@gmail.com>
2025-08-22 11:50:28 +0200

    adjust ui

--- include/ui.h
+++ include/ui.h
@@ -1,6 +1,8 @@
 #ifndef UI_H
 #define UI_H
 
-void prompt_user(int *work, int *breaktime, int *rounds);
+void prompt_user_generic(const char *msg, char *buf, size_t n);
+void prompt_user_name_1(char *buf, size_t n);
+void prompt_user_name_2(char *buf, size_t n);
 
 #endif
--- src/ui.c
+++ src/ui.c
@@ -1,19 +1,26 @@
 #include "ui.h"
 #include <stdio.h>
+#include <string.h>
 
-void prompt_user(int *work, int *breaktime, int *rounds) {
-    printf("\033[2J\033[H"); // clear screen: \033[2J = ANSI escape code to clear the screen; \033[H Moves cursor to the home position (top-left)
-    printf("okay, time to start!\n");
 
-    printf("enter work session duration in minutes: ");
+void prompt_user_generic(const char *msg, char *buf, size_t n) {
+    printf("\033[2J\033[H");
+    printf("%s", msg);
     fflush(stdout);
-    scanf("%d", work);
 
-    printf("enter break duration in minutes: ");
-    fflush(stdout);
-    scanf("%d", breaktime);
+    if (fgets(buf, n, stdin)) {
+        size_t len = strlen(buf);
+        if (len > 0 && buf[len-1] == '\n') buf[len-1] = '\0';
+    } else {
+        buf[0] = '\0';
+    }
+}
 
-    printf("enter number of rounds: ");
-    fflush(stdout);
-    scanf("%d", rounds);
+
+void prompt_user_name_1(char *buf, size_t n) {
+    prompt_user_generic("enter your terminal nickname: ", buf, n);
+}
+
+void prompt_user_name_2(char *buf, size_t n) {
+    prompt_user_generic("somebody wants to talk to you!\nenter your terminal nickname: ", buf, n);
 }
