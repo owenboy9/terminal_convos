7ffdfacaeeca5867bc935ea7f4618ec746e95804
owenboy9 <owenkraus9@gmail.com>
2025-08-22 12:25:04 +0200

    update terminal spawn

--- src/termspaws.c
+++ src/termspaws.c
@@ -9,42 +9,21 @@
 
 extern char **environ;
 
-int spawn_timer_terminal(const char *self_exe, const char *sock_path, int work_min, int break_min, int rounds, pid_t *out_pid) {
+int spawn_chat_terminal(const char *self_exe, const char *sock_path, const char *msg, pid_t *out_pid) {
 
-    char w[16], b[16], r[16];
-    snprintf(w, sizeof(w), "%d", work_min);
-    snprintf(b, sizeof(b), "%d", break_min);
-    snprintf(r, sizeof(r), "%d", rounds);
+    char msg_buf[256];
+    snprintf(msg_buf, sizeof(msg_buf), "%s", msg);
 
     pid_t pid;
     int rc = 0;
 
     // try gnome-terminal
-    const char *term = "gnome-terminal";
-    char *argv[] = {
-        (char*)term, (char*)"--", (char*)self_exe, (char*)"--timer",
-        (char*)sock_path, w, b, r, NULL
-    };
-    rc = posix_spawnp(&pid, term, NULL, NULL, argv, environ);
-
-    if (rc != 0) {
-        // fallback to ubuntu standard terminal
-        term = "x-terminal-emulator";
-        char *argv2[] = {
-            (char*)term, (char*)"-e", (char*)self_exe, (char*)"--timer",
-            (char*)sock_path, w, b, r, NULL
-        };
-        rc = posix_spawnp(&pid, term, NULL, NULL, argv2, environ);
-
-        if (rc != 0) {
-            // fallback to xterm
-            term = "xterm";
-            char *argv3[] = {
-                (char*)term, (char*)"-e", (char*)self_exe, (char*)"--timer",
-                (char*)sock_path, w, b, r, NULL
-            };
-            rc = posix_spawnp(&pid, term, NULL, NULL, argv3, environ);
-        }
+    const char *terms[] = {"gnome-terminal", "x-terminal-emulator", "xterm"};
+    for (int i = 0; i < 3; i++) {
+        const char *term = terms[i];
+        char *argv[] = { (char*)term, (char*)"--", (char*)self_exe, (char*)"--chat", (char*)sock_path, msg_buf, NULL };
+        rc = posix_spawn(&pid, term, NULL, NULL, argv, environ);
+        if (rc == 0) break; // success
     }
 
     if (rc != 0) {
