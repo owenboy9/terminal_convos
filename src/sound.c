85d2e627b4eb35bdc24817166ba31fad50eb5d56
owenboy9 <owenkraus9@gmail.com>
2025-08-25 14:29:42 +0200

    simple solution for sound speed but not valgrind great, i guess

--- src/sound.c
+++ src/sound.c
@@ -6,6 +6,6 @@ const char *NEW_MESSAGE = "sounds/new_message.mp3";
 
 void play_sound(const char *path) {
     char cmd[512];
-    snprintf(cmd, sizeof(cmd), "mpg123 %s > /dev/null 2>&1", path);
+    snprintf(cmd, sizeof(cmd), "mpg123 %s > /dev/null 2>&1 &", path);
     system(cmd);
 }
